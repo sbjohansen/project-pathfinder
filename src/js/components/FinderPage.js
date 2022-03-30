6a81a59e83c3971bfaac5f17729152c56ad08295
Slawomir Johansen <baran@fastmail.com>
2022-03-30 20:54:19 +0200

    REWRITE drawing logic, coords storing, event listeners

--- src/js/components/FinderPage.js
+++ src/js/components/FinderPage.js
@@ -6,11 +6,24 @@ class FinderPage {
   constructor(element){
     const thisFinder = this;
 
+    thisFinder.stage = 1;
+
     thisFinder.render(element);
-    thisFinder.initGrid();   
-    //thisFinder.initDrawing(); 
     thisFinder.initActions();
     
+    thisFinder.grid = {};
+    for(let row = 1; row <= 10; row++) {
+      thisFinder.grid[row] = {};
+      for(let col = 1; col <= 10; col++) {
+        thisFinder.grid[row][col] = false;
+      }
+    }
+
+    thisFinder.clickedGridOrderX = [];
+    thisFinder.clickedGridOrderY = [];
+
+
+
   }
 
 
@@ -31,7 +44,6 @@ class FinderPage {
     thisFinder.dom = {};
     thisFinder.dom.wrapper = element;
     thisFinder.dom.gridContainer = document.querySelector(select.containerOf.gridContainer);
-    thisFinder.clickedGrid = [];
     thisFinder.gridStart = 0;
     thisFinder.gridFinish = 0;
 
@@ -44,27 +56,30 @@ class FinderPage {
     thisFinder.stageTwo = document.querySelector(classNames.finder.stageTwo);
     thisFinder.stageThree = document.querySelector(classNames.finder.stageThree);
 
+    if(thisFinder.stage === 1){
+      thisFinder.initGrid();
+    }
+
   }
 
   initGrid(){
     const thisFinder = this;
-
-    const container = document.querySelector(select.containerOf.gridContainer);
+    const container = thisFinder.dom.gridContainer;
 
     function makeRows(rows, cols) {
       container.style.setProperty(settings.gridRows, rows);
       container.style.setProperty(settings.gridCols, cols);
-      let c = 0;
-      for (c = 0; c < (rows * cols); c++) {
-        let cell = document.createElement('div');
-        //cell.innerText = (c + 1);
-        container.appendChild(cell).className = classNames.finder.gridItem;
-        container.appendChild(cell).setAttribute('id', c + 1 );
+      for (rows = 1; rows <= 10; rows++) {
+        for(cols = 1; cols <= 10; cols++){
+          let cell = document.createElement('div');
+          //cell.innerText = (c + 1);
+          container.appendChild(cell).className = classNames.finder.gridItem;
+          container.appendChild(cell).setAttribute('data-row', rows );
+          container.appendChild(cell).setAttribute('data-col', cols );
+        }
       }
     }
     
-    thisFinder.initDrawing();
-
     makeRows(10, 10);
   }
 
@@ -72,351 +87,108 @@ class FinderPage {
   initActions(){
     const thisFinder = this;
 
-    const startButton = thisFinder.dom.wrapper.querySelector(classNames.finder.buttonStart);
-    const startFinishButton = thisFinder.dom.wrapper.querySelector(classNames.finder.buttonStartFinish);
-    const computeButton = thisFinder.dom.wrapper.querySelector(classNames.finder.buttonCompute);
-
-    thisFinder.buttonWrapper = thisFinder.dom.wrapper.querySelector(select.containerOf.buttonWrapper);
+    //const startButton = thisFinder.dom.wrapper.querySelector(classNames.finder.buttonStart);
+    //const startFinishButton = thisFinder.dom.wrapper.querySelector(classNames.finder.buttonStartFinish);
+    //const computeButton = thisFinder.dom.wrapper.querySelector(classNames.finder.buttonCompute);
 
 
+    
+    
 
-    startButton.addEventListener('click', function(event){
-      //const thisFinder = this;
-      
-      event.preventDefault();
+    if(thisFinder.stage === 1){
+      thisFinder.dom.gridContainer.addEventListener('click', function(event) {
+        event.preventDefault();
 
+        const clickedElement = event.target;
 
-      startButton.classList.remove(classNames.finder.buttonActive);
-      startFinishButton.classList.add(classNames.finder.buttonActive);
+        if(clickedElement.classList.contains(classNames.finder.gridItem)){
 
+          thisFinder.markField(clickedElement);
+        }
+      });
+    }
 
-      thisFinder.stageOne.classList.remove(classNames.finder.buttonActive);
-      thisFinder.stageTwo.classList.add(classNames.finder.buttonActive);
+  }
 
-      thisFinder.lastClicked.classList.replace(classNames.finder.gridItemLastClicked, classNames.finder.gridItemClicked);
+  markField(clickedElement){
+    const thisFinder = this;
 
-      thisFinder.initStartFinish();
+    //console.log(thisFinder.grid);
 
-      thisFinder.dom.gridContainer.removeEventListener('click', thisFinder.initDrawing(event));
+    const clickedField = {
+      row: parseInt(clickedElement.getAttribute('data-row')),
+      col: parseInt(clickedElement.getAttribute('data-col')),
 
-    });
+    };
 
-    startFinishButton.addEventListener('click', function(event){
-      //const thisFinder = this;
-      
-      event.preventDefault();
+    let previouslyClickedElem = '';
 
+    //CHECK if item is marked, and if it's last marked item
 
-      startFinishButton.classList.remove(classNames.finder.buttonActive);
+    if(thisFinder.grid[clickedField.row][clickedField.col]) {
 
-      computeButton.classList.add(classNames.finder.buttonActive);
+      if(clickedField.row === thisFinder.clickedGridOrderX[thisFinder.clickedGridOrderX.length - 1] && thisFinder.clickedGridOrderY[thisFinder.clickedGridOrderY.length - 1] === clickedField.col ){
 
-      thisFinder.initCompute();
-      
-      thisFinder.stageTwo.classList.remove(classNames.finder.buttonActive);
-      thisFinder.stageThree.classList.add(classNames.finder.buttonActive);
+        thisFinder.grid[clickedField.row][clickedField.col] = false;
+        clickedElement.classList.remove(classNames.finder.gridItemLastClicked);
 
-      
-
-    });
-
-    computeButton.addEventListener('click', function(event){
-      //const thisFinder = this;
-      
-      event.preventDefault();
+        thisFinder.clickedGridOrderX.pop();
+        thisFinder.clickedGridOrderY.pop();
 
 
-      computeButton.classList.remove(classNames.finder.buttonActive);
+        //change last clicked item after removing last from array
 
-      startButton.classList.add(classNames.finder.buttonActive);
+        previouslyClickedElem = thisFinder.dom.gridContainer.querySelector('[data-col="' + thisFinder.clickedGridOrderY[thisFinder.clickedGridOrderY.length - 1] + '"]' + '[data-row="' + thisFinder.clickedGridOrderX[thisFinder.clickedGridOrderX.length - 1] + '"]' );
 
-      
 
-      for(let cell of thisFinder.dom.gridContainer.children){
-        cell.classList.remove(classNames.finder.gridItemClicked);
-        cell.classList.remove(classNames.finder.start);
-        cell.classList.remove(classNames.finder.finish);
-        cell.classList.remove(classNames.finder.path);
-
-        thisFinder.gridStart = 0;
-        thisFinder.gridFinish = 0;
-        thisFinder.clickedGrid = [];
-
+        if(previouslyClickedElem !== null){
+          previouslyClickedElem.classList.replace(classNames.finder.gridItemClicked, classNames.finder.gridItemLastClicked);
+        }
       }
+    }
 
-      thisFinder.stageThree.classList.remove(classNames.finder.buttonActive);
-      thisFinder.stageOne.classList.add(classNames.finder.buttonActive);
+    else {
+      const gridValues = Object.values(thisFinder.grid)
+        .map(col => Object.values(col))
+        .flat();
 
-    });
+      if(gridValues.includes(true)) {
+        const edgeFields = [];
+        if(clickedField.col > 1) edgeFields.push(thisFinder.grid[clickedField.row][clickedField.col-1]); //get field on the left value
+        if(clickedField.col < 10) edgeFields.push(thisFinder.grid[clickedField.row][clickedField.col+1]); //get field on the right value
+        if(clickedField.row > 1) edgeFields.push(thisFinder.grid[clickedField.row-1][clickedField.col]); //get field on the top value
+        if(clickedField.row < 10) edgeFields.push(thisFinder.grid[clickedField.row+1][clickedField.col]); //get field on the bottom value
 
+        if(!edgeFields.includes(true)){
+          alert('A new field should touch at least one that is already selected!');
+          return;
+        }
+      }
+    
+      thisFinder.grid[clickedField.row][clickedField.col] = true;
+      clickedElement.classList.add(classNames.finder.gridItemLastClicked);
+
+      //add cords to array for defining last, second last etc
+
+      thisFinder.clickedGridOrderX.push(clickedField.row);
+      thisFinder.clickedGridOrderY.push(clickedField.col);
+
+      previouslyClickedElem = thisFinder.dom.gridContainer.querySelector('[data-col="' + thisFinder.clickedGridOrderY[thisFinder.clickedGridOrderY.length - 2] + '"]' + '[data-row="' + thisFinder.clickedGridOrderX[thisFinder.clickedGridOrderX.length - 2] + '"]' );
+
+      if (previouslyClickedElem !== null){
+        previouslyClickedElem.classList.replace(classNames.finder.gridItemLastClicked, classNames.finder.gridItemClicked);
+      }
+    }
   }
 
 
   
-  initDrawing() {
-    const thisFinder = this;
+ 
 
-    
+ 
 
-    const startButton = thisFinder.dom.wrapper.querySelector(classNames.finder.buttonStart);
-    const startFinishButton = thisFinder.dom.wrapper.querySelector(classNames.finder.buttonStartFinish);
-    thisFinder.action = true;
-
-    //console.log(startButton.classList.contains(classNames.finder.buttonActive));
-    //ROUTE DRAWING EVENT LISTENER
-
-    
-    thisFinder.dom.gridContainer.addEventListener('click', function(event){
-      const clickedElement = event.target;
-
-      const clickedElementId = parseInt(event.target.getAttribute('id'));
-
-      const cellRight = thisFinder.clickedGrid.includes(parseInt(clickedElementId + 1 ));
-      const cellLeft = thisFinder.clickedGrid.includes(parseInt(clickedElementId - 1 ));
-      const cellTop = thisFinder.clickedGrid.includes(parseInt(clickedElementId - 10 ));
-      const cellBottom = thisFinder.clickedGrid.includes(parseInt(clickedElementId + 10 ));
 
   
-      if(clickedElement.getAttribute('class') === classNames.finder.gridItem ){
-        
-
-        if(startButton.classList.contains(classNames.finder.buttonActive) === true){
-        //check if there is any grid active 
-
-          if(thisFinder.clickedGrid.length === 0  && startButton.classList.contains(classNames.finder.buttonActive) === true){
-
-            thisFinder.clickedGrid.push(parseInt(clickedElement.getAttribute('id')));
-
-            clickedElement.classList.add(classNames.finder.gridItemLastClicked);
-            
-            thisFinder.lastClicked = clickedElement;
-            
-          }
-          // if there is grid active check for conditions for next active grid
-
-          else if (thisFinder.clickedGrid.length >= 0 && startButton.classList.contains(classNames.finder.buttonActive) === true) {
-            
-            if (cellRight === true || cellLeft === true || cellBottom === true || cellTop === true ){
-              
-              thisFinder.clickedGrid.push(parseInt(clickedElement.getAttribute('id')));
-
-              thisFinder.lastClicked.classList.replace(classNames.finder.gridItemLastClicked, classNames.finder.gridItemClicked);
-
-              clickedElement.classList.add(classNames.finder.gridItemLastClicked);
-
-              thisFinder.lastClicked = clickedElement;
-              
-
-            }
-          } 
-          if (startFinishButton.classList.contains(classNames.finder.buttonActive) === true) {
-            clickedElement.classList.replace(classNames.finder.gridItemClicked, classNames.finder.start);
-
-          }
-        } 
-
-        //CONDITIONS FOR REMOVING MARKED CELLS
-
-        else if ( clickedElementId === thisFinder.clickedGrid[thisFinder.clickedGrid.length - 1] && startButton.classList.contains(classNames.finder.buttonActive) === true) {
-
-          clickedElement.classList.remove(classNames.finder.gridItemClicked);
-          clickedElement.classList.remove(classNames.finder.gridItemLastClicked);
-
-          const clickedId = thisFinder.clickedGrid.indexOf(parseInt(clickedElement.getAttribute('id')));
-          thisFinder.clickedGrid.splice(clickedId, 1);
-
-          
-          const previousClickedId = thisFinder.clickedGrid[thisFinder.clickedGrid.length - 1];
-
-          thisFinder.lastClicked = document.getElementById(previousClickedId);
-
-
-          if(thisFinder.lastClicked !== null){
-
-            thisFinder.lastClicked.classList.replace(classNames.finder.gridItemClicked, classNames.finder.gridItemLastClicked);
-          }
-        } else {
-          console.log('not last clicked');
-        }
-
-        //END OF ROUTE DRAWING EVENT LISTENER
-
-      }
-    });
-
-  }
-
-  initStartFinish(){
-    const thisFinder = this;
-
-    const startFinishButton = thisFinder.dom.wrapper.querySelector(classNames.finder.buttonStartFinish);
-
-    
-    thisFinder.dom.gridContainer.addEventListener('click', function(event){
-
-      const clickedElement = event.target;
-
-      const clickedElementId = parseInt(event.target.getAttribute('id'));
-    
-      if(clickedElement.classList.contains(classNames.finder.gridItemClicked)){
-        
-        
-        if(startFinishButton.classList.contains(classNames.finder.buttonActive) === true && thisFinder.gridStart === 0){
-        
-          clickedElement.classList.replace(classNames.finder.gridItemClicked, classNames.finder.start);
-          thisFinder.gridStart = clickedElementId;
-
-        } else if (
-          startFinishButton.classList.contains(classNames.finder.buttonActive) === true && 
-        !clickedElement.classList.contains(classNames.finder.start) && 
-        thisFinder.gridFinish === 0 && thisFinder.gridStart !== 0){
-          clickedElement.classList.replace(classNames.finder.gridItemClicked, classNames.finder.finish);
-          thisFinder.gridFinish = clickedElementId;
-
-          //console.log(thisFinder.gridStart, thisFinder.gridFinish);
-        }
-
-        
-      }
-    });
-  }
-
-
-  initCompute(){
-    const thisFinder = this;
-
-    const startXY = thisFinder.gridStart;
-    const finishXY = thisFinder.gridFinish;
-
-    const startElement = document.getElementById(startXY);
-    const finishElement = document.getElementById(finishXY);
-
-    let startY = Math.floor((startXY + 10)/10);
-    let startX = startXY - (Math.floor((startXY)/10)*10);
-
-    let finishY = Math.floor((finishXY + 10)/10);
-    let finishX = finishXY - (Math.floor((finishXY)/10)*10);
-
-
-    //console.log('col', 'Sx', startX,  'Sy', startY);
-    console.log('col', 'Fx', finishX, 'Fy', finishY);
-    
-    
-    if( parseInt(startX) === 0){
-      startX = 10;
-    }
-
-     
-    if( parseInt(finishX) === 0){
-      finishX = 10;
-    }
-    let routeX = '';
-    let routeY = '';
-
-    thisFinder.coordXY = '';
-
-    if(startX > finishX){
-      if( parseInt(startX) === 0){
-        startX = 10;
-      }
-
-    
-      if( parseInt(finishX) === 0){
-        finishX = 10;
-      }
-    
-      routeX = startX - finishX;
-
-     
-
-      for(let i = 0; i < routeX + 1; i++){
-        //console.log('bb');
-        const pathElem = document.getElementById(thisFinder.gridStart - i );
-        //pathElem.classList.remove(classNames.finder.gridItemClicked, classNames.finder.start, classNames.finder.finish);
-        console.log('test');
-        pathElem.classList.add(classNames.finder.path);
-        startElement.classList.replace(classNames.finder.start, classNames.finder.path);
-        finishElement.classList.replace(classNames.finder.finish, classNames.finder.path);
-        console.log('col', 'Sx', startX,  'Sy', startY);
-        console.log('col', 'Fx', finishX, 'Fy', finishY);
-
-      }
-
-    } else if (startX < finishX) {
-      
-      if( parseInt(startX) === 0){
-        startX = 10;
-      }
-      if( parseInt(finishX) === 0){
-        finishX = 10;
-      }
-      
-      routeX = finishX - startX;
-      
-      for(let i = 0 ; i < routeX + 1 ; i++){
-        console.log('bb');
-        const pathElem = document.getElementById(thisFinder.gridStart + i );
-        pathElem.classList.add(classNames.finder.path);
-
-        pathElem.classList.replace(classNames.finder.gridItemClicked, classNames.finder.path);
-        startElement.classList.replace(classNames.finder.start, classNames.finder.path);
-        startElement.classList.replace(classNames.finder.finish, classNames.finder.path);
-
-      }
-    } else {
-      routeX = 9;
-      console.log('route 10');
-    }
- 
-    if(startY > finishY){
-      routeY = startY - finishY;
-
-      console.log(thisFinder.gridFinish);
-
-      for(let i = 0; i < routeY + 1; i++){
-        //console.log('bb');
-        const pathElem = document.getElementById(thisFinder.gridFinish + (i*10));
-        pathElem.classList.replace(classNames.finder.gridItemClicked, classNames.finder.path);
-        pathElem.classList.add(classNames.finder.path);
-
-        console.log(pathElem);
-        finishElement.classList.replace(classNames.finder.finish, classNames.finder.path);
-        startElement.classList.replace(classNames.finder.start, classNames.finder.path);
-
-      }
-      
-
-
-      
-    } else if (startY < finishY) {
-      
-      
-      
-      
-      routeY = finishY - startY;
-
-     
-
-      for(let i = 0; i < routeY + 1; i++){
-        const pathElem = document.getElementById(thisFinder.gridFinish - (i*10));
-        //pathElem.classList.remove(classNames.finder.gridItemClicked, classNames.finder.start, classNames.finder.finish);
-        pathElem.classList.replace(classNames.finder.gridItemClicked, classNames.finder.path);
-        pathElem.classList.add(classNames.finder.path);
-
-        finishElement.classList.replace(classNames.finder.finish, classNames.finder.path);
-        
-      }
-    } else {
-      routeY = 9;
-    }
-
-    //console.log ('routeX:', routeX, 'routeY', routeY);
-
-
- 
-
-    console.log(thisFinder.coordXY);
-  }
 }
 
 
